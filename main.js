be53ff3cf291e8e6bdd526328951f4d181b2afda
AppleHowMuch <SecreTary_RC@hotmail.com>
2023-10-11 12:06:17 +0800

    import & export

---
+++ main.js
@@ -0,0 +1,15 @@
+// 具名匯出
+export const a = 2;
+
+export const text = "家恆很帥";
+
+const obj ={
+    name:"JoJo",
+    age:18,
+}
+
+function fn(){
+    console.log("xxxx")
+}
+// 預設匯出 只能有一個
+export default fn;
